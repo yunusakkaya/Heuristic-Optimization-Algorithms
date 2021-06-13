e25c481ba95b7c2b9111df7053f380fb40719a07
yunusakkaya <57574383+yunusakkaya@users.noreply.github.com>
2021-06-13 09:53:12 +0300

    Update and rename yarilama.py to bisection.py

--- bisection.py
+++ bisection.py
@@ -1,24 +1,24 @@
 from sympy import *
 import numpy as np
 
-w= float(input("üzeri 4 olan terimin katsayısını girin:"))
-a= float(input("üzeri 3 olan terimin katsayısını girin:"))
-b= float(input("üzeri 2 olan terimin katsayısını girin:"))
-d= float(input("üzeri 1 olan terimin katsayısını girin:"))
-c= float(input("sabit terimi girin:"))
+w= float(input("Term coefficient with 4 power of x:"))
+a= float(input("Term coefficient with 3 power of x:"))
+b= float(input("Term coefficient with 2 power of x:"))
+d= float(input("Term coefficient with 1 power of x:"))
+c= float(input("constant term:"))
 
 x = Symbol('x')
 y = w*(x**4) + a*(x**3) + b*(x**2) + d*(x) + c
 ydif = y.diff(x)
 
 print("Y:", y)
-print("Y nin türevi:", ydif)
+print("Diff of Y:", ydif)
 
-alt=float(input("alt sınır girin:"))
-üst=float(input("üst sınır girin:"))
+alt=float(input("lower variable:"))
+üst=float(input("upper variable:"))
 print("aralık:",alt,üst)
 
-iterasyonsayisi= int(input("iterasyon sayısı girin:"))
+iterasyonsayisi= int(input("number of iteration:"))
 
 if (ydif.subs('x',alt))*(ydif.subs('x',üst)) < 0:
     
@@ -27,11 +27,11 @@ if (ydif.subs('x',alt))*(ydif.subs('x',üst)) < 0:
             print(i,".iterasyon=> şu anki orta nokta:",ortanokta)
             if ydif.subs('x',ortanokta)*ydif.subs('x',alt) < 0:
                 üst=ortanokta
-                print("yeni aralık:",alt,",",üst)
+                print("new range:",alt,",",üst)
 
             if ydif.subs('x',ortanokta)*ydif.subs('x',üst) < 0:
                 alt=ortanokta
-                print("yeni aralık:",alt,",",üst)
+                print("new range:",alt,",",üst)
                 
             print("________________________________________-")
             
