91b5775bf45cb5265fdf1c7e6facbc3905c825eb
yunusakkaya <57574383+yunusakkaya@users.noreply.github.com>
2021-06-18 19:13:43 +0300

    Update and rename altinoran.py to goldensection.py

--- goldensection.py
+++ goldensection.py
@@ -5,14 +5,14 @@ w= float(input("üzeri 4 olan terimin katsayısını girin:"))
 a= float(input("üzeri 3 olan terimin katsayısını girin:"))
 b= float(input("üzeri 2 olan terimin katsayısını girin:"))
 d= float(input("üzeri 1 olan terimin katsayısını girin:"))
-c= float(input("sabit terimi girin:"))
+c= float(input(":"))
 
 x = Symbol('x')
 y = w*(x**4) + a*(x**3) + b*(x**2) + d*(x) + c
 
-a=float(input("a yı girin:"))
-b=float(input("b yi girin:"))
-k=int(input("(0.618^^k*(b-a) < E) şeklinde k yı girin: :"))
+a=float(input("a :"))
+b=float(input("b :"))
+k=int(input("iteration number :"))
 
 for i in range (0,k+1):
     x1= b - 0.618*(b-a)
@@ -21,13 +21,13 @@ for i in range (0,k+1):
     
     if y.subs('x',x1) < y.subs('x',x2):
         a= x1
-        print("yeni aralık değerleri:",a,",",b)
-        print("yeni aralık:", (b-a))
+        print("new range values:",a,",",b)
+        print("new range:", (b-a))
         
     if y.subs('x',x1) > y.subs('x',x2):
         b= x2
-        print("yeni aralık değerleri:",a,",",b)
-        print("yeni aralık:", (b-a))
+        print("new range values:",a,",",b)
+        print("new range:", (b-a))
         
     print("_________________________________________")    
     
